82d3ff95e2f3cd917e9972733d8c7b75be36dc32
Chris <chris@builder.lan>
2023-03-22 16:30:37 -0400

    show config images for debugging

--- apply-config.sh
+++ apply-config.sh
@@ -58,6 +58,8 @@ else
   AVAILABLE_IMAGES=($(run show container image | awk '{ if ( NR > 1  ) { print $1 ":" $2} }'))
   CONFIG_IMAGES=($(sed -nr "s/set container name .* image '(.*)'/\1/p" /config/config-parts/* | uniq))
 
+  echo $CONFIG_IMAGES
+
   for image in "${CONFIG_IMAGES[@]}"
   do
     if [[ ! " ${AVAILABLE_IMAGES[*]} " =~ " ${image} " ]]; then
