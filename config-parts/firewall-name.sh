5b42e02f6dd521362bb2e50c84fd74ece1063056
Chris <chris@builder.lan>
2023-03-22 16:51:46 -0400

    Revert "disable wan ssh"
    This reverts commit 15b1c866238e9db056fb68f0e48f7b8d3ed7db4b.

--- config-parts/firewall-name.sh
+++ config-parts/firewall-name.sh
@@ -113,6 +113,10 @@ set firewall name wan-local rule 1 action 'accept'
 set firewall name wan-local rule 1 description 'Rule: accept_wireguard'
 set firewall name wan-local rule 1 destination port '51820'
 set firewall name wan-local rule 1 protocol 'udp'
+set firewall name wan-local rule 2 action 'accept'
+set firewall name wan-local rule 2 description 'Rule: accept_ssh'
+set firewall name wan-local rule 2 destination port 'ssh'
+set firewall name wan-local rule 2 protocol 'tcp'
 
 # From WAN to SERVICES
 set firewall name wan-services default-action 'drop'
