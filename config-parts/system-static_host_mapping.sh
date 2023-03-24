aca2aa8fbdd70200ccc5251f462a9bcd7eb3d454
Chris <chris@builder.lan>
2023-03-24 11:06:53 -0400

    update static mappings

--- config-parts/system-static_host_mapping.sh
+++ config-parts/system-static_host_mapping.sh
@@ -19,6 +19,11 @@ set system static-host-mapping host-name sakura.ctec.run inet 10.0.0.12
 set system static-host-mapping host-name rem.ctec.run inet 10.0.0.13
 set system static-host-mapping host-name ram.ctec.run inet 10.0.0.14
 
+# LXD Containers
+set system static-host-mapping host-name minio.ctec.run inet 10.0.0.97
+set system static-host-mapping host-name flexo.ctec.run inet 10.0.0.98
+set system static-host-mapping host-name garage.ctec.run inet 10.0.0.99
+
 # Kubernetes cluster VIP
 set system static-host-mapping host-name cluster.ctec.run inet 10.0.0.9
 
