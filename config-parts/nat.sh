ca0782d19b4d83dc1d722104ed72b1bdfe79eebd
Chris <chris@builder.lan>
2023-03-23 20:59:05 -0400

    force all devices to use dnsdist

--- config-parts/nat.sh
+++ config-parts/nat.sh
@@ -15,6 +15,15 @@ set nat destination rule 101 protocol 'tcp'
 set nat destination rule 101 translation address ${INGRESS_IP}
 set nat destination rule 101 translation port '80'
 
+# Force DNS
+set nat destination rule 102 description 'Force DNS for LAN'
+set nat destination rule 102 destination address '!10.5.0.4'
+set nat destination rule 102 destination port '53'
+set nat destination rule 102 inbound-interface 'eth1'
+set nat destination rule 102 protocol 'tcp_udp'
+set nat destination rule 102 translation address '10.5.0.4'
+set nat destination rule 102 translation port '53'
+
 # LAN -> WAN masquerade
 set nat source rule 100 description 'LAN -> WAN'
 set nat source rule 100 destination address '0.0.0.0/0'
